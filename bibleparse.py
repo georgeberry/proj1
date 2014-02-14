488216b07090689f9c0f4a4f92f309a305baac5b
George Berry <geb97@cornell.edu>
2014-02-14 12:40:33 -0500

    more stuff

--- bibleparse.py
+++ bibleparse.py
@@ -12,7 +12,7 @@ import random
 #open file
 biblefile = '/Users/georgeberry/Google Drive/Spring 2014/CS5740/proj1/bible_corpus 2/kjbible.train'
 
-with open(biblefile, 'rb') as f:
+with open(biblefile, 'r') as f:
     bible = f.read()
 
 
@@ -21,7 +21,7 @@ def timer(f):
     def wrapper(*args,**kwargs):
         tic = time.time()
         result = f(*args, **kwargs)
-        print f.__name__ + " took " + str(time.time() - tic) + " seconds"
+        print(f.__name__ + " took " + str(time.time() - tic) + " seconds")
         return result
     return wrapper
 
@@ -50,6 +50,7 @@ def lookup_default(list, index, default):
         return list[index]
     except:
         return default
+
 
 #classes
 
@@ -80,15 +81,55 @@ class gram:
         except:
             self.next_words[word] = 1
 
-    def random_next(self):
-        self.cum_sum = sum(w for c, w in self.next_words.iteritems())
+    def next(self, word_dict):
+        self.cum_sum = sum(w for c, w in word_dict.iteritems())
         r = random.uniform(0, self.cum_sum)
         left_point = 0
-        for c, w in self.next_words.iteritems():
+        for c, w in word_dict.iteritems():
             if left_point + w > r:
                 return c
             left_point += w
         assert False, "wtf"
+
+    def random_next(self):
+        return self.next(self.next_words)
+
+    @staticmethod
+    def seen_words(word_dict):
+        return set(c for c, w in word_dict.iteritems())
+
+    @staticmethod
+    def count(word_dict):
+        return sum(w for c, w in word_dict.iteritems())
+
+    #smooth
+    #idea here: w_n-1 has occured a times
+    #we can get this from summing the total occurances of following words, w
+    #then, we make a choice:
+    #pick a word in the "seen words" dict with probability, a+set(elements of a)/(a+V), then choose word normally
+    #OR, with prob 1 - a+set(elements of a)/(a+V), pick a word uniformly at random from the words we HAVEN'T seen
+    def laplace_next(self, words_in_vocab):
+        laplace = self.next_words
+
+        for key, val in laplace.iteritems():
+            laplace[key] += 1
+
+        cut_point = self.count(laplace)/(self.count(self.next_words) + len(words_in_vocab))
+
+        r = random.uniform(0, self.count(self.next_words) + len(words_in_vocab))
+
+        if r < cut_point:
+            return self.next(laplace)
+        else: 
+            return random.choice(list(set(words_in_vocab) - self.seen_words(laplace)))
+
+
+    def phrase_probability(self, phrase_as_tuple):
+        if not isinstance(phrase_as_tuple, tuple):
+            return 'nope'
+
+         
+
 
     def __repr__(self):
         return 'gram for previous words: ' + str(self.prev_words)
@@ -103,6 +144,7 @@ class ngrams:
     def __init__(self, n, text_as_list):
         self.n = n
         self.text = text_as_list
+        self.vocab = set(text_as_list)
         self.conditionals = {}
 
         self.process()
@@ -111,6 +153,7 @@ class ngrams:
     def process(self):
         word_range = range(self.n-1, -1, -1)
 
+        #make unsmoothed
         for token_num in range(len(self.text)):
             temp = list()
 
@@ -126,39 +169,74 @@ class ngrams:
                 self.conditionals[tuple(temp)] = gram(tuple(temp), current_word)
 
     @timer
-    def gen(self):
+    def gen(self, laplace_smoothing = False):
         sentence = []
 
-        begin_candidates = []
+        if self.n > 1:
+            begin_candidates = []
 
-        for k, v in self.conditionals.iteritems():
-            if k[0] == '<s>':
-                begin_candidates.append(k)
+            for k, v in self.conditionals.iteritems():
+                if k[0] == '<s>':
+                    begin_candidates.append(k)
 
-        current = random.choice(begin_candidates)
-        sentence.append(current[1])
-
+            current = random.choice(begin_candidates)
+            for word in current:
+                sentence.append(word)
+        else:
+            sentence.append(self.conditionals[()].random_next())
 
         #print begin_candidates
         #print self.conditionals
         #if self.n == 1:
         #    current = '<s>'
 
-        while current[1] != '.' and current[1] != '?' and current[1] != '!':
-            s = self.conditionals[current].random_next()
-            sentence.append(s)
-            current = (sentence[-2], sentence[-1])
+        while sentence[-1] != '.' and sentence[-1] != '?' and sentence[-1] != '!':
+            if self.n > 1:
+                if laplace_smoothing == False:
+                    s = self.conditionals[tuple(current)].random_next()
+                else:
+                    try:
+                        s = self.conditionals[tuple(current)].laplace_next(self.vocab)
+                    except: #maybe our n-1 gram isn't in the text. assume all words are equally likely
+                        #reduces to 1/V, or a random choice from the vocab words
+                        s = random.choice(list(self.vocab))
 
+                sentence.append(s)
+                current = sentence[-self.n+1:]
+            else:
+                if laplace_smoothing == False:
+                    s = self.conditionals[()].random_next() #for unigram
+                else:
+                    s = self.conditionals[()].laplace_next(self.vocab)
+                sentence.append(s)
+                current = tuple()
+                
         return ' '.join(sentence)
 
 
+    def perplexity(self, corpus_as_list):
+
+
+
+
+
+
 if __name__ == "__main__":
-
     t = clean_up(bible)
-    tt = ngrams(3, t)
+    tt = ngrams(1, t)
 
-    for each in range(5):
-        print tt.gen() + '\n'
+    for each in range(10):
+        print('~~~~~')
+        tt.gen() + '\n'
+
+    tt = ngrams(2, t)
+    for each in range(10):
+        tt.gen(laplace_smoothing = True) + '\n'
+
+    tt = ngrams(3, t)
+    for each in range(10):
+        tt.gen(tt.gen(laplace_smoothing = True)) + '\n'
+
 
 
 '''
